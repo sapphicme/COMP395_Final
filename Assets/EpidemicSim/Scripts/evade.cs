f82d5caf71d98006c4a4188c8a2f4708530fff81
Karem <karemalani2402@yahoo.com>
2021-04-04 16:31:02 -0400

    added enemy evading from the player

---
+++ Assets/EpidemicSim/Scripts/evade.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class evade : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+    public GameObject player;
+    public float EnemyDistanceRun = 4.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, player.transform.position);
+
+        Debug.Log("distance:" + distance);
+
+        if(distance < EnemyDistanceRun)
+        {
+            Vector3 dirToPlayer = transform.position - player.transform.position;
+
+            Vector3 newPos = transform.position + dirToPlayer;
+
+            _agent.SetDestination(newPos);
+
+
+        }
+        
+    }
+}
---
+++ Assets/EpidemicSim/Scripts/score.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class score : MonoBehaviour
+{
+    public static int scoreValue = 0;
+    Text playerScore;
+
+    private void Start()
+    {
+        playerScore = GetComponent<Text>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        playerScore.text = "Score: " + scoreValue;
+        
+    }
+}
