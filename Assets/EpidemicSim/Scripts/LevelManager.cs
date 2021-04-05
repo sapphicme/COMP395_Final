e81784126c73016cee8f563df4a1effce093acf1
Arielle Mueller <43253070+sapphicme@users.noreply.github.com>
2021-04-04 21:17:37 -0400

    LevelManager + Menu

---
+++ Assets/EpidemicSim/Scripts/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LevelManager : MonoBehaviour
+{
+    public void LoadNewScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
