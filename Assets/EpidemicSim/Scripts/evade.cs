fe1039a77813fcfb40cfd9f7fa2e4a8b6cee9551
Exzeed <geethan1996@hotmail.ca>
2021-04-15 04:01:34 -0400

    Added some things (rushed)
    -Added basic logic to AI for getting infected and dieing (and visual indications)
    -Fixed UI to properly display score (and added placeholder for number of healthy/infected/dead people)
    -Added AI with model (disabled currently) though evade script makes it move in T-pose

--- Assets/EpidemicSim/Scripts/evade.cs
+++ Assets/EpidemicSim/Scripts/evade.cs
@@ -8,6 +8,11 @@ public class evade : MonoBehaviour
     private NavMeshAgent _agent;
     public GameObject player;
     public float EnemyDistanceRun = 3.0f;
+    public Material infected;
+    public Material dead;
+    private bool isInfected = false;
+    private double countdown;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,20 +23,39 @@ public class evade : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float distance = Vector3.Distance(transform.position, player.transform.position);
-
-        Debug.Log("distance:" + distance);
-
-        if(distance < EnemyDistanceRun)
+        if (!isInfected)
         {
-            Vector3 dirToPlayer = transform.position - player.transform.position;
+            float distance = Vector3.Distance(transform.position, player.transform.position);
 
-            Vector3 newPos = transform.position + dirToPlayer;
+            Debug.Log("distance:" + distance);
 
-            _agent.SetDestination(newPos);
+            if(distance < EnemyDistanceRun)
+            {
+                Vector3 dirToPlayer = transform.position - player.transform.position;
 
+                Vector3 newPos = transform.position + dirToPlayer;
 
+                _agent.SetDestination(newPos);
+
+            }
         }
-        
+
+        if (isInfected)
+        {
+            countdown += Time.deltaTime;
+            if(countdown >= 10)
+            {
+                this.gameObject.GetComponent<Renderer>().material = dead;
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            this.gameObject.GetComponent<Renderer>().material = infected;
+            isInfected = true;
+        }
     }
 }
